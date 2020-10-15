19b8ae1c2755f3af739856f0b8002ca6a66ad1c8
Simon Funke <simon.funke@gmail.com>
2020-10-15 09:01:23 +0200

    Create mp3 files

--- trim.sh
+++ trim.sh
@@ -1,3 +1,8 @@
-THRESH=72
-
-ffmpeg -i Lillehagveien\ 95B.m4a -af "silenceremove=start_periods=1:start_duration=1:start_threshold=-${THRESH}dB:detection=peak,aformat=dblp,areverse,silenceremove=start_periods=1:start_duration=1:start_threshold=-${THRESH}dB:detection=peak,aformat=dblp,areverse" 1.m4a
+ffmpeg -i "Lillehagveien 95B.m4a" 1.mp3
+ffmpeg -i "Lillehagveien 95B 2.m4a" 2.mp3
+ffmpeg -i "Lillehagveien 95B 3.m4a" 3.mp3
+ffmpeg -i "Lillehagveien 95B 4.m4a" 4.mp3
+ffmpeg -i "Lillehagveien 95B 5.m4a" 5.mp3
+ffmpeg -i "Lillehagveien 95B 6.m4a" 6.mp3
+ffmpeg -i "Lillehagveien 95B 7.m4a" 7.mp3
+ffmpeg -i "Lillehagveien 95B 8.m4a" 8.mp3
